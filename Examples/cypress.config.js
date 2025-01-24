13e94e7fb912857cfc4ae811003fd3eab909e789
Damascustogah007 <Damascustogah007@gmail.com>
2025-01-24 13:08:52 +0000

    yaml file update 2

--- Examples/cypress.config.js
+++ Examples/cypress.config.js
@@ -1,17 +1,5 @@
 const { defineConfig } = require("cypress");
 
-const ENV_CONFIG = {
-  dev: {
-    baseUrl: 'http://localhost:3000',
-  },
-  staging: {
-    baseUrl: 'https://staging.example.com',
-  },
-  production: {
-    baseUrl: 'https://www.example.com',
-  },
-}
-
 module.exports = defineConfig({
   env: {
     baseUrl: 'http://localhost:3000',
@@ -29,7 +17,6 @@ module.exports = defineConfig({
     chromeWebSecurity : false,
     waitForAnimations : true,
     specPattern : 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    baseUrl: ENV_CONFIG[process.env.ENV]?.baseUrl || 'http://localhost:3000', // Default to localhost
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
