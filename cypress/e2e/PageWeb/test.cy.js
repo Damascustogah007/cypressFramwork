bcd86057097faed666d6122ca097ad2044b27e79
Damascustogah007 <Damascustogah007@gmail.com>
2025-01-29 11:22:49 +0000

    Added parallelization to gitAction

--- cypress/e2e/PageWeb/test.cy.js
+++ cypress/e2e/PageWeb/test.cy.js
@@ -3,7 +3,6 @@ describe('test', () => {
         cy.viewport(Cypress.env('viewportWidth'), Cypress.env('viewportHeight'));
     });
     it('Should visit google homepage homepage', () => {
-        const url = Cypress.env('baseUrl')
-        cy.visit(url)
+        cy.visit('https://www.google.com')
     });
 });
